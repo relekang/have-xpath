677a02fd6147a09acc7e0bf37eab8d2da5dd5fbd
Rolf Erik Lekang <me@rolflekang.com>
2017-05-23 20:53:31 +0200

    chore: Add requestAnimationFrame polyfill

--- test/setup.js
+++ test/setup.js
@@ -1,10 +1,28 @@
 /* eslint-env node, browser */
 import jsdom from 'jsdom';
 
+const polyfill = (() => {
+  let clock = Date.now();
+
+  return (callback) => {
+    const currentTime = Date.now();
+    if (currentTime - clock > 16) {
+      clock = currentTime;
+      callback(currentTime);
+    } else {
+      setTimeout(() => {
+        polyfill(callback);
+      }, 0);
+    }
+  };
+})();
+
 if (typeof global.document === 'undefined') {
   const dom = new jsdom.JSDOM('<!doctype html><html><body></body></html>');
   global.document = dom.window.document;
   global.window = dom.window;
+  global.window.requestAnimationFrame = polyfill;
+  global.requestAnimationFrame = polyfill;
 }
 
 global.navigator = window.navigator;
